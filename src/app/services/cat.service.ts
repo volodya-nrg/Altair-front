206b2f18db31b107d4a87968c75f77061336abb8
vladimirumerov <volodya-nrg@mail.ru>
2020-04-19 17:05:49 +0300

    refactor(prod): hotfix

--- src/app/services/cat.service.ts
+++ src/app/services/cat.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {environment} from '../../environments/environment';
 import {HttpClient} from '@angular/common/http';
-import {CatTreeInterface} from '../interfaces/response/cat';
+import {CatInterface, CatTreeInterface} from '../interfaces/response/cat';
 import {Observable} from 'rxjs';
 
 @Injectable({
@@ -15,29 +15,19 @@ export class CatService {
     ) {
     }
 
-    getTree(): Observable<CatTreeInterface> {
-        return this.http.get<CatTreeInterface>(`${this.url}/api/v1/cats`, {
+    getList(): Observable<CatInterface> {
+        return this.http.get<CatInterface>(`${this.url}/api/v1/cats`, {
             params: {
-                asTree: 'true',
-                isFillPropertiesFull: 'false',
+                asTree: 'false',
             }
         });
     }
 
-    // getList(): Observable<CatInterface> {
-    //     return this.http.get<CatInterface>(`${this.url}/api/v1/cats`, {
-    //         params: {
-    //             asTree: 'false',
-    //             isFillPropertiesFull: 'false',
-    //         }
-    //     });
-    // }
-    // getTreeFull(): Observable<CatTreeFullInterface> {
-    //     return this.http.get<CatTreeFullInterface>(`${this.url}/api/v1/cats`, {
-    //         params: {
-    //             asTree: 'true',
-    //             isFillPropertiesFull: 'true',
-    //         }
-    //     });
-    // }
+    getTree(): Observable<CatTreeInterface> {
+        return this.http.get<CatTreeInterface>(`${this.url}/api/v1/cats`, {
+            params: {
+                asTree: 'true'
+            }
+        });
+    }
 }
