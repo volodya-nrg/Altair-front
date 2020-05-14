2178fc9e1838dce9b1d6fed5f358d33d13c65124
vladimirumerov <volodya-nrg@mail.ru>
2020-05-14 13:46:30 +0300

    refactor: forms cats 2

--- src/app/components/forms-cats/forms-cats-get-cats-catid/forms-cats-get-cats-catid.component.spec.ts
+++ src/app/components/forms-cats/forms-cats-get-cats-catid/forms-cats-get-cats-catid.component.spec.ts
@@ -1,25 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { FormsCatsGetCatsCatidComponent } from './forms-cats-get-cats-catid.component';
+import {FormsCatsGetCatsCatidComponent} from './forms-cats-get-cats-catid.component';
 
 describe('FormsCatsGetCatsCatidComponent', () => {
-  let component: FormsCatsGetCatsCatidComponent;
-  let fixture: ComponentFixture<FormsCatsGetCatsCatidComponent>;
+    let component: FormsCatsGetCatsCatidComponent;
+    let fixture: ComponentFixture<FormsCatsGetCatsCatidComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ FormsCatsGetCatsCatidComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [FormsCatsGetCatsCatidComponent]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FormsCatsGetCatsCatidComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(FormsCatsGetCatsCatidComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
