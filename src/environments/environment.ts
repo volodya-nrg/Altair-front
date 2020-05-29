ef4806fe0068830ef24642fbc1e7c876f69fb8b0
vladimirumerov <volodya-nrg@mail.ru>
2020-05-29 19:02:25 +0300

    refactor: remove comment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,7 +12,6 @@ export const environment = {
     domain: 'http://localhost:4200',
     ymapsPathScript: 'https://api-maps.yandex.ru/2.1/?apikey=777a18a8-eb0d-4f9b-ae88-99aed55c227b&lang=ru_RU',
     youTubeExampleLink: 'https://www.youtube.com/watch?v=zU-LndSG5RE',
-    // походу массивы тут нельзя ставить
 };
 
 /*
